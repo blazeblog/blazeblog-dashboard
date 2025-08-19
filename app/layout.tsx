0211bc1218d094982561d4a9621cc0ba4d2c927f
kushagra <giantkillerkushagra@gmail.com>
2025-08-20 01:59:26 +0530

    feat: Add Tawk.to chat integration script and update deploy script in package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,21 @@ export default function RootLayout({
             {children}
             <Toaster />
           </ThemeProvider>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/68a3602b304673192629b883/1j2v2vbvj';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          />
         </body>
       </html>
     </ClerkProvider>
